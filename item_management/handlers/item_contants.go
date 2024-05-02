e0b20a35123a2861009bc219abba5b6bbbf80b7b
beingrohanpandit <rohansharma12434@gmail.com>
2024-05-02 21:48:44 +0530

    feat(item_management): Establish database connection and setup the basic structure

---
+++ item_management/handlers/item_contants.go
@@ -0,0 +1,7 @@
+package handlers
+
+import (
+	"example.com/models"
+)
+
+type MyApp models.App
--- item_management/main.go
+++ item_management/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"example.com/handlers"
+	"example.com/repositories"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+
+	// Get the local (true or false) and convert it into bool value
+	local, _ := strconv.ParseBool(os.Getenv("local"))
+
+	// Initialize the gin router
+	r := gin.Default()
+
+	// Connect to the database
+	db, err := repositories.ConnectDb(local)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Initialize an instance of the application with the database connection
+	app := handlers.MyApp{
+		DB: db,
+	}
+
+	// Define routes for the application
+	app.Routes(r)
+
+	// If running locally then start the server
+	if local {
+		r.Run(":8080")
+	}
+
+}
+
+func main() {
+
+}
